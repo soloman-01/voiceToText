0e17b1b313811e355637962e9186027ad9d70be3
soloman-01 <el99sharawy@gmail.com>
2020-08-24 16:42:53 +0545

    First Commit

---
+++ app/src/main/java/com/example/speechtotext/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.speechtotext;
+
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Locale;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView outputText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        outputText = (TextView) findViewById(R.id.txvResult);
+    }
+
+    public void btnSpeech(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi, Speak Something.");
+
+        try {
+            startActivityForResult(intent, 1);
+        }catch (ActivityNotFoundException e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+
+            case 1:
+                if (resultCode==RESULT_OK && null!=data){
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    outputText.setText(result.get(0));
+                }
+                break;
+        }
+    }
+}
